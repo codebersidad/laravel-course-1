39f0be596b89adba61b922dff36438d0fcade2bb
vagrant <vagrant@homestead>
2020-04-21 02:37:13 +0000

    part 2

---
+++ resources/views/include/nav.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+          {{ config('app.name', 'Blog') }}
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('home') }}">{{ __('Home') }}</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/blog">{{ __('Blog') }}</a>
+            </li>
+          </ul>
+
+        </div>
+    </div>
+</nav>
